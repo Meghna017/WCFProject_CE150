fc05ac4e585f66cdfe70184c100e26598076519a
Meghna017 <74093650+Meghna017@users.noreply.github.com>
2021-02-22 08:02:09 +0530

    Uploaded project files
    Uploaded project files

---
+++ LibrarySystemService/LibrarySystemService/Book.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace LibrarySystemService
+{
+    [DataContract]
+    public class Book
+    {
+        //private int cid;
+        //private string name;
+        //private string type;
+        //private string cno;
+        [DataMember]
+        public int BookId
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string BookName
+        {
+            get;
+            set;
+        }
+        
+        [DataMember]
+        public string ISBN
+        {
+            get;
+            set;
+        }
+
+        [DataMember]
+        public int Price
+        {
+            get;
+            set;
+        }
+    }
+
+}
---
+++ LibrarySystemService/LibrarySystemService/IService1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+namespace LibrarySystemService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        List<Book> GetBooks();
+
+        [OperationContract]
+        List<Book> GetBook(int id);
+
+        [OperationContract]
+        List<Book> GetBookByName(string carname);
+        
+        [OperationContract]
+        string AddStudent(int id,string Name, string Email, string Address, string Password);
+        [OperationContract]
+        string AddBook(int id, string Name, int price,int ISBN);
+
+        [OperationContract]
+        string AddStudentBook(StudentBook uc);
+
+        [OperationContract]
+        List<StudentBook> GetStudentBooks(int uid);
+
+        [OperationContract]
+        StudentBook GetStudentBook(int id);
+
+        [OperationContract]
+        string UpdateStudentBook(StudentBook uc);
+
+        [OperationContract]
+        string DeleteStudentBook(int id);
+        
+        [OperationContract]
+        string DeleteBook(int id);
+
+    }
+
+
+}
